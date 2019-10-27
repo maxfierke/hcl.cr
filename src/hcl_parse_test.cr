d8e95f3aea7bbec70118fc019d9e68d87d811434
Max Fierke <max@maxfierke.com>
2019-10-27 00:57:57 -0500

    Enable pegmatite tracing in hcl_parse_test

--- src/hcl_parse_test.cr
+++ src/hcl_parse_test.cr
@@ -26,14 +26,38 @@ data "a_single_datum" { foo = "bar" }
 
 HEREDOC
 
-pp! Pegmatite.tokenize(HCL::Grammar, SRC_TEXT)
+puts "#" * 80
+puts "START PEGMATITE"
+puts "#" * 80
+Pegmatite.tokenize(HCL::Grammar, SRC_TEXT, io: STDOUT)
+puts "#" * 80
+puts "END PEGMATITE"
+puts "#" * 80
 
 parser = HCL::Parser.new(SRC_TEXT)
 tokens = parser.parse
+puts "#" * 80
+puts "START TOKEN DUMP"
+puts "#" * 80
 pp! tokens
+puts "#" * 80
+puts "END TOKEN DUMP"
+puts "#" * 80
 
 values = tokens.map { |token| token.value }
+puts "#" * 80
+puts "START TOKEN VALUE DUMP"
+puts "#" * 80
 pp! values
+puts "#" * 80
+puts "END TOKEN VALUE DUMP"
+puts "#" * 80
 
 string = tokens.map { |token| token.string }.join('\n')
+puts "#" * 80
+puts "START TOKEN STRING DUMP"
+puts "#" * 80
 puts string
+puts "#" * 80
+puts "END TOKEN STRING DUMP"
+puts "#" * 80
