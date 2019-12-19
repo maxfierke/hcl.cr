91efceb49314eaa226741e5d0318d0bdd0640642
Max Fierke <max@maxfierke.com>
2019-12-18 21:54:37 -0600

    Use cleaner implementation of HCL::Any.new for JSON::PullParser
    Avoids use of macros and a shadow type tuple

--- src/hcl/value_type.cr
+++ src/hcl/value_type.cr
@@ -15,18 +15,6 @@ module HCL
       Hash(String, RawType) |
       Array(RawType)
 
-    # :nodoc:
-    # TODO: Figure out a way to do this in a less gross way
-    TypeTuple = {
-      Nil,
-      Bool,
-      String,
-      Int64,
-      Float64,
-      Hash(String, Any),
-      Array(Any)
-    }
-
     @raw : Type
 
     getter :raw
@@ -223,42 +211,34 @@ module HCL
 
     # Reads a `HCL::Any` value from the given pull parser.
     #
-    # Based on https://github.com/crystal-lang/crystal/blob/6952aacb37682558d1a976b4ebf1b1456d9f8b84/src/json/from_json.cr#L226
+    # Based on https://github.com/crystal-lang/crystal/blob/6952aacb37682558d1a976b4ebf1b1456d9f8b84/src/json/any.cr#L23
     def self.new(pull : JSON::PullParser)
-      location = pull.location
-
-      # Optimization: use fast path for primitive types
-      {% begin %}
-        # Here we store types that are not primitive types
-        {% non_primitives = [] of Nil %}
-
-        {% for type, index in TypeTuple %}
-          {% if type == Nil %}
-            return pull.read_null if pull.kind.null?
-          {% elsif type == Bool || type == Int64 || type == Float64 || type == String %}
-            value = pull.read?({{type}})
-            return Any.new(value) unless value.nil?
-          {% else %}
-            {% non_primitives << type %}
-          {% end %}
-        {% end %}
-
-        # If after traversing all the types we are left with just one
-        # non-primitive type, we can parse it directly (no need to use `read_raw`)
-        {% if non_primitives.size == 1 %}
-          return Any.new({{non_primitives[0]}}.new(pull))
-        {% end %}
-      {% end %}
-
-      string = pull.read_raw
-      {% for type in TypeTuple %}
-        begin
-          return Any.new({{type}}.from_json(string))
-        rescue JSON::ParseException
-          # Ignore
+      case pull.kind
+      when .null?
+        new(pull.read_null)
+      when .bool?
+        new(pull.read_bool)
+      when .int?
+        new(pull.read_int)
+      when .float?
+        new(pull.read_float)
+      when .string?
+        new(pull.read_string)
+      when .begin_array?
+        ary = [] of HCL::Any
+        pull.read_array do
+          ary << new(pull)
         end
-      {% end %}
-      raise JSON::ParseException.new("Couldn't parse #{self} from #{string}", *location)
+        new(ary)
+      when .begin_object?
+        hash = {} of String => HCL::Any
+        pull.read_object do |key|
+          hash[key] = new(pull)
+        end
+        new(hash)
+      else
+        raise "Unknown pull kind: #{pull.kind}"
+      end
     end
 
     # :nodoc:
