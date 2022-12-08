de2e9defdbacb812e38ff39937d58c42f8a2988b
Max Fierke <max@maxfierke.com>
2022-12-08 13:53:27 -0600

    capture additional blocks in RootSpec

--- src/hcldec/spec/root_spec.cr
+++ src/hcldec/spec/root_spec.cr
@@ -8,15 +8,49 @@ module HCLDec
     @[HCL::Block]
     property object : ObjectSpec? = nil
 
+    @[HCL::Block(key: "block")]
+    property blocks : Array(HCLDec::BlockSpec) = Array(HCLDec::BlockSpec).new
+
+    @[HCL::Block(key: "block_list")]
+    property block_lists : Array(HCLDec::BlockListSpec) = Array(HCLDec::BlockListSpec).new
+
+    @[HCL::Block(key: "function")]
+    property functions : Array(HCLDec::UserFuncSpec) = Array(HCLDec::UserFuncSpec).new
+
+    @[HCL::Block]
+    property variables : HCLDec::VariablesSpec? = nil
+
     def validate!
-      if !literal && !object
+      found_blocks = 0
+      found_blocks += 1 if literal
+      found_blocks += 1 if object
+      found_blocks += 1 if blocks.any?
+      found_blocks += 1 if block_lists.any?
+
+      if found_blocks == 0
         raise SpecViolation.new(
           "Missing spec block: A spec file must have exactly one root block specifying how to map to a JSON value."
         )
-      elsif literal && object
+      elsif found_blocks > 1
         raise SpecViolation.new(
           "Extraneous spec block: A spec file must have exactly one root block specifying how to map to a JSON value."
         )
+      end
+
+      if lit = literal
+        lit.validate!
+      end
+
+      if obj = object
+        obj.validate!
+      end
+
+      blocks.each(&.validate!)
+      block_lists.each(&.validate!)
+      functions.each(&.validate!)
+
+      if vars = variables
+        vars.validate!
       end
     end
   end
